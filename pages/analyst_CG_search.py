0f1b3ae8dc75b1899b0c312e90afd7f5c7a54fda
Jaime Diaz <jaime.2.diaz@gm.com>
2025-09-04 09:47:30 -0400

    playing with a possible search bar to search for GC

--- pages/analyst_CG_search.py
+++ pages/analyst_CG_search.py
@@ -9,13 +9,15 @@ st.sidebar.success("You are currently viewing the CG search page")
 Add an editable table to the page to show all CG records with a search box.
     call a function from cg_processing.py to get the data
 
-Use the cleaned headers from cg_processing.py
-
 Try to make all sql calls determined in the processing scripts to keep this page clean.
 
 Ask Jim exactly what he wants on this page.
 
 Make it possible to create a new CG record from this page?
 """
+
+cg_search = st.text_input("Movie title", "Enter CG tracking ID to search")
+st.write (f"Searching for CG tracking ID: {cg_search}")
+
 data = base_cg_query()
 st.dataframe(data)
