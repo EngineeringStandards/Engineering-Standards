786d8e1bf7f485f3bcacf7c1fa1d53d7292646f3
Jaime Diaz <jaime.2.diaz@gm.com>
2025-09-04 11:11:25 -0400

    adding measures to make sure the dataframe is always populated

--- pages/analyst_CG_search.py
+++ pages/analyst_CG_search.py
@@ -19,9 +19,9 @@ Make it possible to create a new CG record from this page?
 cg_search = st.text_input("CG Tracking ID", "Enter CG tracking ID to search")
 st.write (f"Searching for CG tracking ID: {cg_search}")
 
-if cg_search != "Enter CG tracking ID to search":
+if cg_search != "Enter CG tracking ID to search" or cg_search != "":
     data = find_CG_by_tracking_id(cg_search)
 else:
     data = base_cg_query()
-    
+
 st.dataframe(data)
