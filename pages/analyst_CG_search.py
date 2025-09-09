d785ed5b3c933d86bc42e6d2c6d2e9710b4debf2
Jaime Diaz <jaime.2.diaz@gm.com>
2025-09-09 08:47:03 -0400

    doubled width

--- pages/analyst_CG_search.py
+++ pages/analyst_CG_search.py
@@ -38,7 +38,7 @@ else:
     edited_data = st.data_editor(
         data,
         key="cg_editor", 
-        width=2500,
+        width=5000,
         hide_index=True,
         num_rows="dynamic",
         disabled=["Tracking ID"]
