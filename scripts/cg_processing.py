a88235600055283447902959b76946879a486d2e
Jaime Diaz <jaime.2.diaz@gm.com>
2025-09-10 14:44:16 -0400

    renamed the function to old name

--- scripts/cg_processing.py
+++ scripts/cg_processing.py
@@ -82,7 +82,7 @@ Find a specific CG record by using its Tracking ID.
 Parameters:
     tracking_id (str): The CG Tracking ID to search for.
 """
-def find_CG(tracking_id: str):
+def find_CG_by_tracking_id(tracking_id: str):
     # Prepare and execute SQL query to find the CG record
     search_query = f"SELECT {base_columns} FROM maxis_sandbox.engineering_standards.cg_cleaned_data WHERE tracking_id = '{tracking_id}'"
     df = sqlQuery(search_query)
